dd2d0c3815bbaf5b35bebdd2224b7aa8561986a7
Chandan Yadav <chandanyadav5844@gmail.com>
2024-11-30 16:50:05 +0530

    fifth

---
+++ src/data/ipuzData.js
@@ -0,0 +1,122 @@
+const ipuzData = {
+    
+    
+    version: 'http://ipuz.org/v1',
+    kind: ['http://ipuz.org/crossword#1'],
+    
+    dimensions: { width:15 , height: 15 },
+    
+ 
+  
+ puzzle : [
+  [1, 0, 2, 0, 3, '#', 4, 0, 5, '#', 6, 0, 0, 7, 0],
+  [0, '#', '0', '#', 0, '#', 0, '#', 0, '#', 0, '#', '#', 0, '#'],
+  [
+    0, '#', 8, { cell: 0, style: { shapebg: 'circle' } }, '#',
+    9, { cell: 0, style: { shapebg: 'circle' } }, '#', 10, 0, 
+    0, '#', '#', 0, '#'
+  ],
+  [
+    '#', 11, 0, '#', 12, '#', '#', '#', 0, '#', '#', 
+    { cell: 13, style: { shapebg: 'circle' } }, 0, 0, 14
+  ],
+  ['#', '#', '#', '#', 0, '#', '#', '#', '#', '#', '#', 0, '#', '#', 0],
+  [15, 0, 0, 0, '#', 16, 0, 0, '#', 17, 0, 0, 0, '#', 0],
+  [0, '#', '#', '#', '#', 0, '#', '#', '#', '#', '#', 0,  '#', '#', 0],
+  ['#', 18, 0, 19, '#', 20, 0, 21, 0, 22, '#', '#', 23, 0, 0],
+  [
+    24, '#', '#', { cell: 0, style: { shapebg: 'circle' } }, '#',
+    0, '#', 0, '#', 0, 0, { cell: 25, style: { shapebg: 'circle' } }, '#', '#', '#'
+  ],
+  [0, '#', '#', 0, '#', 0, '#', 0, '#', 0, '#', 26, 0, '#', '#'],
+  [27, 0, '#', 0, '#', '#', '#', 0, '#', '#', '#', 0, '#', '#', '#'],
+  [0, '#', '#', 0, '#', '#', '#', '#', '#', '#', '#', 28, 0, 0, 0],
+  ['#', 29, '#', 30, 31, 0, 0, '#', 32, 0, 33, '#', '#', '#', '#'],
+  [
+    34, 0,  { cell: 0, style: { shapebg: 'circle' } }, '#',
+     0, '#', '#', '#', '#', '#', '0', '#', '#', '#', 35
+  ],
+  ['#', 0, '#', 36, 0, 0, 0, '#', 37, 0, 0, 0, 0, '#', 0],
+  
+],
+
+ clues : {
+  Across: [
+    [1, 'Create a new Directory'],
+    [4, 'Display a Calender'],
+    [6, 'Change file Permission'],
+    [8, "Bring the Job to the foreground"],
+    [9, 'Routing devices and tunnels'],
+    [10, 'Manipulate shell variables and functions '],
+    [11, "Display free disk space"],
+    [13, 'Search file in a directory hierarchy'],
+    [15, 'Display or change the Date & Time'],
+    [16, 'List Process running on the system'],
+    [17, 'Search file for lines that match a given pattern'],
+    [18, 'Briefly list Directory Content'],
+    [20, 'List users currently logged in'],
+    [23, 'DNS Lookup'],
+    [26, 'Print user and group id'],
+    [27, 'List Information about file'],
+    [28, 'Compressing files to save space'],
+    [30, 'Output the last part of a file'],
+    [32, 'Copy files between two machine'],
+    [32, 'Copy files between two machine'],
+    [34, 'Reverse lines of file'],
+    [36, 'Display file or file system status'],
+    [37, 'Remove  Directory'],
+   
+
+  ],
+
+  Down: [
+    [1, 'Help manual'],
+    [2, 'Display the differences between two file'],
+    [3, 'Remove files or directory'],
+    [4, 'Compare two files'],
+    [5, "List Information about file"],
+    [6, "Devide a file into several parts"],
+    [7, 'Open a file in its default Application'],
+    [12, "Substitute user"],
+    [13, "Display Memory Uses"],
+    [14, 'Print kernal & driver message'],
+    [15, 'Estimate file Space usage'],
+    [16, 'Create a empty file'],
+    [19, 'Reboot the system'],
+    [21, 'Display message on screen'],
+    [22, 'Secure Shell client (remote login Program'],
+    [24, "Display help for a built in command"],
+    [25, "Test a network connection"],
+    [29, 'Print numeric sequance'],
+
+    [31, 'Search for and install software package'],
+    [33, 'Print Working Directory'],
+    [35, 'Change Directory'],
+    
+  ],
+},
+
+ solution : [
+  ['M', 'K', 'D', 'I', 'R', '#', 'C', 'A', 'L', '#', 'C', 'H', 'M', 'O', 'D'],
+  ['A', '#', 'I', '#', 'M', '#', 'M', '#', 'I', '#', 'U', '#', '#', 'P', '#'],
+  ['N', '#', 'F', 'G', '#', 'I', 'P', '#', 'S', 'E', 'T', '#', '#', 'E', '#'],
+  ['#', 'D', 'F', '#', 'S', '#', '#', '#', 'T', '#', '#', 'F', 'I', 'N', 'D'],
+  ['#', '#', '#', '#', 'U', '#', '#', '#', '#', '#', '#', 'R', '#', '#', 'M'],
+  ['D', 'A', 'T', 'E', '#', 'T', 'O', 'P', '#', 'G', 'R', 'E', 'P', '#', 'E'],
+  ['U', '#', '#', '#', '#', 'O', '#', '#', '#', '#', '#', 'E', '#', '#', 'S'],
+  ['#', 'D', 'I', 'R', '#', 'U', 'S', 'E', 'R', 'S', '#', '#', 'D', 'I', 'G'],
+  ['H', '#', '#', 'E', '#', 'C', '#', 'C', '#', 'S', '#', 'P', '#', '#', '#'],
+  ['E', '#', '#', 'B', '#', 'H', '#', 'H', '#', 'H', '#', 'I', 'D', '#', '#'],
+  ['L', 'S', '#', 'O', '#', '#', '#', 'O', '#', '#', '#', 'N', '#', '#', '#'],
+  ['P', '#', '#', 'O', '#', '#', '#', '#', '#', '#', '#', 'G', 'Z', 'I', 'P'],
+  ['#', 'S', '#', 'T', 'A', 'I', 'L', '#', 'R', 'C', 'P', '#', '#', '#', '#'],
+  ['R', 'E', 'V', '#', 'P', '#', '#', '#', '#', '#', 'W', '#', '#', '#', 'C'],
+  ['#', 'Q', '#', 'S', 'T', 'A', 'T', '#', 'R', 'M', 'D', 'I', 'R', '#', 'D'],
+]
+
+};
+ 
+
+  
+  export default ipuzData;
+  
